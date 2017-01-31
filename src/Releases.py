afbe82cc905b8bc53c754155839bebecab9f0585
hussamh10 <hussamh10@gmail.com>
2017-01-31 21:27:51 +0500

    Added releases module

--- src/Releases.py
+++ src/Releases.py
@@ -6,9 +6,8 @@ def getProductURL(url):
     html = urllib.request.urlopen(url)
 
     soup = BeautifulSoup(html, "html.parser")
-    product_url = soup.find_all('a')[29].get('href')
 
-    print(product_url)
+    product_url = soup.find_all('a')[104].get('href')
     return product_url
 
 def find(string, char):
@@ -19,35 +18,24 @@ def getStats(url):
         return "Sorry, nothing found."
 
     url = 'http://www.releases.com' + url
-
     html = urllib.request.urlopen(url)
     soup = BeautifulSoup(html, "html.parser")
-    soup = soup.findAll("div", attrs={"class" : " p-rl2-item"})
+    soup = soup.findAll("div", attrs={"class" : " p-rl-autotext"})
 
-    items = []
+    description = ''
     for a in soup:
-        items.append(a.getText(separator=u'$'))
+        description += a.getText(separator=u'$')
 
-    print(items)
-
-    for item in items:
-        if ('[Confirmed]' in item):
-            item = item.replace('$[Confirmed]', '')
-        if ('Track$Track' in item):
-            item = item.replace('Track$Track', '')
-        if ('-' in item):
-            dash_index = find(item, '-')[-1]
-            item = item[:dash_index]
-        else :
-            item = item.rstrip('0123456789$')
-            item = item.rstrip('0123456789')
-            item = item+'$'
-        return item
+    return description
 
 def getReleaseDate(ext):
-    ext = ext.replace(' ', '+')
+    ext = ext.replace(' ', '%20')
     main_url = 'http://www.releases.com/search?q='
     url = main_url + ext
+    print(url)
+
     product_url = getProductURL(url)
+
+    print(product_url)
 
     return getStats(product_url)
